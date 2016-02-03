32cd2174b6fa092078c32e97297d384d1ae3e577
Donghyun Kim <kdh0102@postech.ac.kr>
2016-02-03 22:39:41 +0900

    data 분할하는 코드 적혀잇어요

--- cite_blackboard/member_list.cpp
+++ cite_blackboard/member_list.cpp
@@ -4,9 +4,32 @@ member_list::member_list(){}
 
 member_list::~member_list(){}
 
+int member_list::get_num(){
+	return num;
+}
+
+void member_list::set_num(int new_num){
+	num = new_num;
+}
+
 void member_list::read_file(){
 	//initialize lists
-	
+	num = 0;
+	while(!_list.empty()){
+		_list.pop_back();
+	}
+	/*
+	while(!gr_list.empty()){
+	gr_list.pop_back();
+	}
+	while(!staff_list.empty()){
+	staff_list.pop_back();
+	}
+	while(!faculty_list.empty()){
+	faculty_list.pop_back();
+	}
+	*/
+
 	//read file
 	const string filename = "data.txt";
 	string line;
@@ -15,7 +38,6 @@ void member_list::read_file(){
 	ifstream myfile(filename.c_str());
 	if (myfile.is_open()){ 
 		while ( getline (myfile,line) ){
-
 			if(line.find("-교원") != string::npos)
 				mode = _faculty;
 			else if(line.find("-직원") != string::npos)
@@ -43,41 +65,95 @@ void member_list::read_file(){
 	else cout << "존재하지 않는 파일 이름입니다." << endl;
 
 }
-/*
+
+
 void member_list::parse_file(string line, int mode){
 	
 	int num_of_info = 0;
 	string temp = "";
+	member * new_member;
+
+	//variables for 'string to int'
+	//stringstream s1;
+	int i1;
+
+	//allocate new member according to mode
+	switch(mode){
+		case _undergraduate:	new_member = new undergraduate;
+								_list.push_back(new_member);
+								break;
+		/*
+		case _graduate:			new_member = new graduate;
+								_list.push_back(new_member);
+								break;
+		case _staff:			new_member = new staff;
+								_list.push_back(new_member);
+								break;
+		case _faculty:			new_member = new faculty;
+								_list.push_back(new_member);
+								break;
+		*/
+	}
+
+	//'it'(iterator) directs 'num'th element in '_list'
+	std::list<member*>::iterator it = _list.begin();
+	std::advance(it, num);
+
+	//parse data & insert data
 	for(int i = 0; i<line.size(); i++){
 		if (line[i] == ' ' || i == line.size()-1){
-			if(mode == 0){ //faculty
-				switch(num_of_info){
-				case 0 : faculty_list
-					temp = "";
-					num_of_info++;
-					break;
-				case 1 : person->modify_id(temp);
-					temp = "";
-					num_of_info++;
-					break;
-				case 2 : person->modify_department(temp);
-					temp = "";
-					num_of_info++;
-					break;
-				case 3 : person->modify_position(temp);
-					temp = "";
-					num_of_info++;
-					break;
-				case 4 : temp.push_back(line[i]);
-					person->modify_mail(temp);
-					temp = "";
-					num_of_info++;
-					break;
-				}
+			switch(num_of_info){
+			case 0 : 
+				i1 = atoi(temp.c_str());
+				(*it)->set_id(i1);
+				temp = "";
+				num_of_info++;
+				break;
+			case 1 : (*it)->set_name(temp);
+				temp = "";
+				num_of_info++;
+				break;
+			case 2 : (*it)->set_image(temp);
+				temp = "";
+				num_of_info++;
+				break;
+			case 3 : (*it)->set_birthday(parse_birthday(temp));
+				temp = "";
+				num_of_info++;
+				break;
+			case 4 : temp.push_back(line[i]);
+				(*it)->set_email(temp);
+				temp = "";
+				num_of_info++;
+				break;
 			}
+			
 		}
 		else temp.push_back(line[i]);
 	}
 	num++;
 }
-*/
+
+
+BD member_list::parse_birthday(string birthday){
+	BD temp;
+	int convert;
+
+	//month comes first
+	for(int i = 0; i < 2; i++){
+		string _temp;
+		_temp.push_back(birthday[i]);
+		convert = atoi(_temp.c_str());
+	}
+	temp.month = convert;
+
+	//date comes first
+	for(int i = 2; i < 4; i++){
+		string _temp;
+		_temp.push_back(birthday[i]);
+		convert = atoi(_temp.c_str());
+	}
+	temp.date = convert;
+	
+	return temp;
+}
--- cite_blackboard/member_list.h
+++ cite_blackboard/member_list.h
@@ -10,14 +10,21 @@ class faculty;
 
 class member_list {
 	private:
-		std::list <undergraduate>	ug_list;
+		int num;
+	//supervise list using member pointer
+		std::list <member*>	_list;
 		//std::list <graduate>		gr_list;
 		//std::list <staff>			staff_list;
 		//std::list <faculty>			faculty_list;
 	public:
 		member_list();
 		~member_list();
-		void read_file();
-		void parse_file(string line, int mode);
+		//set,get methods
+		int get_num();
+		void set_num(int new_num);
+		//file
+		void	read_file();
+		void	parse_file(string line, int mode);
+		BD		parse_birthday(string birthday);
 };
 #endif
